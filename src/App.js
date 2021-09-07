444b07e2f93704f6c5697311d11e346148e945d1
Adnan Khan <adnan7.zain@gmail.com>
2021-09-07 19:51:53 +0500

    final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
- 
+import React, { useState } from 'react';
+import List from './List';
+import data from './data';
 
 function App() {
+  const [people, setPeople] = useState(data);
   return (
-    <div className="App">
-       <h2>reminder project setup</h2>
-    </div>
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
   );
 }
 
